afab34ee4038416f9af3995d837977a864276976
Chrapkoj <jakubchrapko@seznam.cz>
2026-01-28 21:04:52 +0100

    pridavam hudbu

--- HrachMatejkaJezekChrapko/gamefiles/assets/audio.py
+++ HrachMatejkaJezekChrapko/gamefiles/assets/audio.py
@@ -1,1 +1,19 @@
 # ZDE BUDOU AUDIO SOUBORY A NASTAVENÍ PRO HERNÍ AUDIO
+import pygame
+import os
+
+# Zjistíme cestu ke složce assets, kde je tento soubor
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def hraj_hudbu():
+    # Cesta k souboru: assets/hudba/menu.mp3
+    cesta = os.path.join(BASE_DIR, "hudba", "menu.mp3")
+    
+    if os.path.exists(cesta):
+        pygame.mixer.init()
+        pygame.mixer.music.load(cesta)
+        pygame.mixer.music.set_volume(0.3) # Hlasitost na 30 %
+        pygame.mixer.music.play(-1)        # -1 bude hrát pořád dokola
+        print("Hudba úspěšně spuštěna!")
+    else:
+        print(f"Chyba: Hudba nenalezena na cestě: {cesta}")
--- HrachMatejkaJezekChrapko/gamefiles/src/main.py
+++ HrachMatejkaJezekChrapko/gamefiles/src/main.py
@@ -1,7 +1,12 @@
 # Hlavní kod pro spuštění herní smyčky
+import os
+import sys
 
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+from assets.audio import hraj_hudbu
 from game_loop import Game
 
 if __name__ == "__main__":
+    hraj_hudbu() 
     Game().run()
