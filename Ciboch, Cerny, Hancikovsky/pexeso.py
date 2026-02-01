1f887b3d42c3224520b505624f484c40ea364f80
zedthe3rd <121316184+polopes@users.noreply.github.com>
2026-02-01 16:29:30 +0100

    První základ hry pexeso

---
+++ Ciboch, Cerny, Hancikovsky/pexeso.py
@@ -0,0 +1,16 @@
+import pygame
+import random
+import time
+
+pygame.init()
+
+# herni settings
+WIDTH, HEIGHT = 600, 600
+ROWS, COLS = 4, 4
+TILE_SIZE = WIDTH // COLS
+
+# barvy
+WHITE = (255, 255, 255)
+GRAY = (200, 200, 200)
+BLUE = (100, 149, 237)
+GREEN = (0, 200, 0)
