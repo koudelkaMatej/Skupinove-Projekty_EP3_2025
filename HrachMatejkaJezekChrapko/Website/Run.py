013058410621e0458305a89102b50a4e4b7719cd
Jonikii <jonasmatej3@gmail.com>
2026-03-23 17:40:40 +0100

    stranky_about_login_contacts

--- HrachMatejkaJezekChrapko/Website/Run.py
+++ HrachMatejkaJezekChrapko/Website/Run.py
@@ -9,20 +9,16 @@ app = Flask(__name__)
 def home():
     return render_template("indexhome.html")
 
-@app.route('/home')
-def home():
-    return render_template("indexhome.html")
+@app.route('/login')
+def login():
+    return render_template("login.html")
 
-@app.route("/about")
+@app.route('/about')
 def about():
     return render_template("about.html")
 
-@app.route("/login")
-def about():
-    return render_template("login.html")
-
-@app.route("/contact")
-def about():
+@app.route('/contact')
+def contact():
     return render_template("contact.html")
 
 
