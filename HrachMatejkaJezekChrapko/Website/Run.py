7fc161b9df7bf2ec663622d10bdb48d3aa7338d7
Jonikii <jonasmatej3@gmail.com>
2026-03-23 16:42:07 +0100

    homepage

--- HrachMatejkaJezekChrapko/Website/Run.py
+++ HrachMatejkaJezekChrapko/Website/Run.py
@@ -1,1 +1,13 @@
-from flask import Flask
+#zakladni importy pro flask
+from flask import Flask, render_template, request, redirect, url_for  
+
+app = Flask(__name__)
+
+
+#domovska stranka
+@app.route('/')
+def home():
+    return render_template("indexhome.html")
+
+if __name__ == '__main__':
+    app.run()
